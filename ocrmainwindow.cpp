a8ad403b6ad908f815648c14b5c1b83797f7697c
poolooloo <poolooloo@gmail.com>
2019-11-15 18:07:51 +0800

    commit picture

--- ocrmainwindow.cpp
+++ ocrmainwindow.cpp
@@ -1,7 +1,9 @@
 #include "ocrmainwindow.h"
 #include "ui_ocrmainwindow.h"
-#include "ocr.h"
 #include <QDebug>
+#include <QFileDialog>
+#include "ocr.h"
+
 
 OcrMainWindow::OcrMainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -10,28 +12,28 @@ OcrMainWindow::OcrMainWindow(QWidget *parent) :
     ui->setupUi(this);
 
     // 设置APPID/AK/SK
-    std::string app_id = "17700105";//"你的 App ID";
-    std::string api_key = "uYeTS7OjmmCTdVemItxxexXB";//"你的 Api key";
-    std::string secret_key = "0nGKHsNaaHDrsDI6Fxn5tGD2WTk8KxZW";//"你的 Secret Key";
+//    std::string app_id = "17700105";//"你的 App ID";
+//    std::string api_key = "uYeTS7OjmmCTdVemItxxexXB";//"你的 Api key";
+//    std::string secret_key = "0nGKHsNaaHDrsDI6Fxn5tGD2WTk8KxZW";//"你的 Secret Key";
 
-    aip::Ocr client(app_id, api_key, secret_key);
+//    client = new aip::Ocr(app_id, api_key, secret_key);
 
 //用户向服务请求识别某张图中的所有文字
-    Json::Value result;
+//    Json::Value result;
 
-    std::string image;
-    int ret = aip::get_file_content("grpc.jpg", &image);
-    qDebug()<<"ret = "<<ret;
+//    std::string image;
+//    int ret = aip::get_file_content("grpc.jpg", &image);
+//    qDebug()<<"ret = "<<ret;
     // 调用通用文字识别, 图片参数为本地图片
-    result = client.general_basic(image, aip::null);
+//    result = client.general_basic(image, aip::null);
 //    qDebug()<<result.getComment(Json::commentBefore);
 //    Json::Reader reader;
 //    for (int index=0; index < result.size(); ++index) {
 //        qDebug()<<result[index]->asString();
 //    }
 
-    std::string outStr = result.toStyledString();
-    qDebug()<<QString::fromStdString(outStr);
+//    std::string outStr = result.toStyledString();
+//    qDebug()<<QString::fromStdString(outStr);
     // 如果有可选参数
 //    std::map<std::string, std::string> options;
 //    options["language_type"] = "CHN_ENG";
@@ -44,4 +46,38 @@ OcrMainWindow::OcrMainWindow(QWidget *parent) :
 OcrMainWindow::~OcrMainWindow()
 {
     delete ui;
+}
+
+void OcrMainWindow::on_btnOpenImg_clicked()
+{
+    mImgUrl = QFileDialog::getOpenFileUrl(0,"打开图片",QUrl("."));
+    if( ! mImgUrl.isEmpty() )
+    {
+        qDebug()<<mImgUrl.toString();
+        qDebug()<<mImgUrl.path();
+        ui->label->setPixmap(QPixmap(mImgUrl.path()));
+    }
+}
+
+void OcrMainWindow::on_btnAnalyzeImg_clicked()
+{
+//    // 设置APPID/AK/SK
+//    std::string app_id = "17700105";//"你的 App ID";
+//    std::string api_key = "uYeTS7OjmmCTdVemItxxexXB";//"你的 Api key";
+//    std::string secret_key = "0nGKHsNaaHDrsDI6Fxn5tGD2WTk8KxZW";//"你的 Secret Key";
+
+    aip::Ocr client(app_id, api_key, secret_key);
+
+//用户向服务请求识别某张图中的所有文字
+    Json::Value result;
+
+    std::string image;
+    int ret = aip::get_file_content(mImgUrl.toString().toLocal8Bit().data(), &image);
+    qDebug()<<"ret = "<<ret;
+    // 调用通用文字识别, 图片参数为本地图片
+    result = client.general_basic(image, aip::null);
+
+
+    std::string outStr = result.toStyledString();
+    qDebug()<<QString::fromStdString(outStr);
 }
--- ocrmainwindow.h
+++ ocrmainwindow.h
@@ -3,6 +3,8 @@
 
 #include <QMainWindow>
 
+#include <QUrl>
+
 namespace Ui {
 class OcrMainWindow;
 }
@@ -15,8 +17,19 @@ public:
     explicit OcrMainWindow(QWidget *parent = 0);
     ~OcrMainWindow();
 
+private slots:
+    void on_btnOpenImg_clicked();
+
+    void on_btnAnalyzeImg_clicked();
+
 private:
     Ui::OcrMainWindow *ui;
+    QImage mImg;
+    QUrl mImgUrl;
+    std::string app_id;
+    std::string api_key;
+    std::string secret_key;
+//    aip::Ocr *client;
 };
 
 #endif // OCRMAINWINDOW_H
